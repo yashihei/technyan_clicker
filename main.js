890de298c2a31132d39aa969fa86ec4e2bc37cb2
yashihei <yashihei@gmail.com>
2013-09-24 04:52:12 +0900

    beta ver

--- main.js
+++ main.js
@@ -8,6 +8,8 @@ var SCREEN_CENTER_Y = SCREEN_HEIGHT/2;
 
 var ASSETS = {
     "tekunyan" : "tekunyan.png",
+    "tekunyan_little" : "tekunyan_little.png",
+    "background" : "background.png"
 };
 
 tm.main(function() {
@@ -17,41 +19,71 @@ tm.main(function() {
     app.fitWindow();    // 自動フィット
     // app.enableStats();
 
-    var loading = tm.app.LoadingScene({
-        width: SCREEN_WIDTH,
-        height: SCREEN_HEIGHT,
-        assets: ASSETS,
-        nextScene: MainScene,
+    var scene = app.currentScene;
+    tm.asset.AssetManager.load(ASSETS);
+
+    var background = tm.app.Sprite("background");
+    background.setPosition(SCREEN_CENTER_X, SCREEN_CENTER_Y);
+    scene.addChild(background);
+
+    var tekuMiniGroup = tm.app.CanvasElement();
+    scene.addChild(tekuMiniGroup);
+
+    var teku = Teku();
+    scene.addChild(teku);
+
+    var bar = tm.app.Shape(SCREEN_WIDTH, 50);
+    bar.setPosition(SCREEN_CENTER_X, teku.y - 197);
+    bar.canvas.clearColor("hsla(0, 0%, 20%, 0.7)");
+    scene.addChild(bar);
+
+    var num = 0;
+    scene.fromJSON({
+        children: [
+            {
+                type: "Label", name: "scoreLabel",
+                text: "0technyans",
+                x: SCREEN_CENTER_X,
+                y: teku.y - 200,
+                fillStyle: "#eee",
+                fontSize: 50,
+                align: "center",
+                baseline: "middle",
+            },
+        ]
     });
-    app.replaceScene(loading);
+
+    teku.onpointingstart = function() {
+        num++;
+        console.log(num);
+        scene.scoreLabel.text = num;
+        scene.scoreLabel.text += "technyans";
+        document.title = num + " technyans - TechnyanClicker";
+
+        var tekuMini = TekuMini().addChildTo(tekuMiniGroup);//addChildTo:parentに自分を子どもとして追加
+        tekuMini.setPosition(tm.util.Random.randint(0, SCREEN_WIDTH), 0);
+        tekuMini.speed = tm.util.Random.randint(2, 5);
+    };
+
+    scene.update = function () {
+    };
 
     app.run();//実行
 });
 
-var MainScene = tm.createClass({
-    superClass : tm.app.Scene,
-    teku: null,
-    num: null,
-    init : function () {
-        this.superInit();
-        this.teku = new Teku();
-        this.addChild(this.teku);
-        this.num = 0;
-
-        self = this;
-        this.teku.onpointingstart = function () {
-            self.num++;
-            console.log(self.num);
-        };
-    }
-});
-
-var Teku = tm.createClass({
+tm.define("Teku", {
     superClass : tm.app.Sprite,
     init : function () {
         this.superInit("tekunyan", 300, 300);
         this.setInteractive(true);
+        this.setBoundingType("circle");
         this.setPosition(SCREEN_CENTER_X, SCREEN_CENTER_Y);
+    },
+    onmousedown : function () {
+        this.setSize(this.width / 1.1, this.height / 1.1);
+    },
+    onmouseup : function () {
+        this.setSize(this.width * 1.1, this.height * 1.1);
     },
     onpointingover : function () {
         this.setSize(this.width * 1.1, this.height * 1.1);
@@ -61,3 +93,17 @@ var Teku = tm.createClass({
     },
 });
 
+tm.define("TekuMini", {
+    superClass : tm.app.Sprite,
+    speed : 0,
+    init : function () {
+        this.superInit("tekunyan_little");
+        this.setInteractive(true);
+        this.x = 0;
+        this.y = 0;
+    },
+    update : function() {
+        this.y += this.speed;
+        if (this.y > SCREEN_HEIGHT) this.isUpdate = false;
+    },
+});
